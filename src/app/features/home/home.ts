8f057b10bf0d934c4446c2d631fc719a6382db0d
nagymarten <nagy.martin19@gmail.com>
2025-10-11 14:25:01 +0200

    Update home page

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { Header } from '../../shared/components/header/header';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [TranslateModule, Header],
+  imports: [TranslateModule, Header, ButtonModule],
   standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.css',
